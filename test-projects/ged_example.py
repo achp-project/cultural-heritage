2a10a003cc08f64b212dca9c4b4aac2b61e5ab76
zoometh <thomashuet7@gmail.com>
2023-07-20 13:34:58 +0100

    readme

--- test-projects/ged_example.py
+++ test-projects/ged_example.py
@@ -13,20 +13,12 @@ G1.add_nodes_from([1, 2, 3, 4])
 G2.add_nodes_from([1, 2, 3, 4])
 
 # %%
-#### Add edges and edges properties
-### G1
+### G1 ###############################################
 G1.add_edges_from([(1, 2), (2, 3), (3, 4)])
-## attributes
-# edges
-G1_attrs_edges = {(0, 1): {"property": 'is identified by'}, (1, 2): {"property": 'was influenced by'}, (2, 3): {"property": 'has note'}, (3, 4):{"property": 'has time-span'}}
+G1_attrs_edges = {(1, 2): {"property": 'P3'}, (2, 3): {"property": 'P4'}, (3, 4):{"property": 'P5'}}
 nx.set_edge_attributes(G1, G1_attrs_edges)
-# nodes
-G1_attrs_nodes = {1: {"entity": 'E39 Actors'}, 2: {"entity": 'E55 Types'}, 3:{"entity": 'E2 Temporal Entities'}, 4:{"entity": 'E7 Activity'}}
+G1_attrs_nodes = {1: {"entity": 'E39'}, 2: {"entity": 'E55'}, 3:{"entity": 'E2'}, 4:{"entity": 'E7'}}
 nx.set_node_attributes(G1, G1_attrs_nodes)
-# G1.node[1]['entity'] = 'E39 Actors'
-# G1.node[2]['entity'] = 'E55 Types'
-# G1.node[3]['entity'] = 'E2 Temporal Entities'
-# G1.node[4]['entity'] = 'E53 Places'
 
 # %%
 p = nx.spring_layout(G1)
@@ -37,16 +29,19 @@ nx.draw_networkx_edge_labels(G1, pos=p, edge_labels = G1_labels_edges)
 
 # %%
 
-### G2
+### G2 ########################################################
 G2.add_edges_from([(1, 2), (2, 3), (4, 3)])
-## attributes
-# edges
-attrs_edges_G2 = {(0, 1): {"property": 'consists of'}, (2, 3): {"property": 'has note'}, (4, 3):{"property": 'has time-span'}}
-nx.set_edge_attributes(G1, attrs_edges_G2)
-# nodes
+G2_attrs_edges = {(1, 2): {"property": 'P3'}, (2, 3): {"property": 'P1'}, (4, 3):{"property": 'P5'}}
+nx.set_edge_attributes(G2, G2_attrs_edges)
+G2_attrs_nodes = {1: {"entity": 'E39'}, 2: {"entity": 'E55'}, 3:{"entity": 'E2'}, 4:{"entity": 'E7'}}
+nx.set_node_attributes(G2, G2_attrs_nodes)
 
-attrsG1 = {(0, 1): {"property": 20}, (2, 3): {"property": 3}, (3, 4):{"property": 3}}
-
+# %%
+# p = nx.spring_layout(G2)
+G2_labels_nodes = nx.get_node_attributes(G2, 'entity')
+G2_labels_edges = nx.get_edge_attributes(G2, 'property')
+nx.draw(G2, pos=p, labels = G2_labels_nodes, with_labels = True)
+nx.draw_networkx_edge_labels(G2, pos=p, edge_labels = G2_labels_edges)
 
 # %%
 # Calculate the number of common edges between G1 and G2
