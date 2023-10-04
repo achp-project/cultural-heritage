c6a27effb6327fe08142964934d8690de1b9b524
zoometh <thomashuet7@gmail.com>
2023-10-04 15:12:30 +0100

    rm ea

--- data/rm/nodes_id.py
+++ data/rm/nodes_id.py
@@ -16,28 +16,19 @@ url = "https://raw.githubusercontent.com/achp-project/prj-eamena-marea/main/reso
 pattern = r'achp-project/(.*?)/main'
 project = re.search(pattern, url)
 project_name = project.group(1) + "-rm-nodes.tsv"
+print(project_name)
 
 response = requests.get(url)
 graph_data = json.loads(response.text)
 root_node_id = graph_data['graph'][0]['root']['nodeid']
-# Create an empty DataFrame
 df_nodes = pd.DataFrame(columns=['rm_node_name', 'rm_node_uuid'])  
-
-
-# %%
 for i in range(1, len(graph_data['graph'][0]['nodes'])):
-    # new_row = {
-    #     'rm_node_name': graph_data['graph'][0]['nodes'][i]['name'],
-    #     'rm_node_uuid': graph_data['graph'][0]['nodes'][i]['nodeid']
-    # }
     new_row = [graph_data['graph'][0]['nodes'][i]['name'], graph_data['graph'][0]['nodes'][i]['nodeid']]
-    print(i)
-    print(new_row)
     df_nodes.loc[i] = new_row
-    # df_nodes = df_nodes.append(new_row, ignore_index=True)
 
 outDir = os.path.dirname(os.path.realpath(__file__))
-file_path = outDir + project_name
+file_path = outDir + '\\' + project_name
+print(file_path)
 df_nodes.to_csv(file_path, sep='\t', index=False)
 
 # len(graph_data['graph'][0]['cards'])
