b47d18bc770156c1984176d262c85e57dc201f3e
zoometh <thomashuet7@gmail.com>
2023-10-04 13:30:56 +0100

    rm ea

--- data/rm/nodes_id.py
+++ data/rm/nodes_id.py
@@ -1,6 +1,9 @@
 
 
 # %%
+# load packages
+
+import os
 import requests
 import json
 import pandas as pd
@@ -8,24 +11,33 @@ import re
 
 # %%
 url = "https://raw.githubusercontent.com/achp-project/prj-eamena-marea/main/resource_models/Heritage%20Place.json"
+
+# %%
+pattern = r'achp-project/(.*?)/main'
+project = re.search(pattern, url)
+project_name = project.group(1) + "-rm-nodes.tsv"
+
 response = requests.get(url)
 graph_data = json.loads(response.text)
 root_node_id = graph_data['graph'][0]['root']['nodeid']
-print(root_node_id)
-
 # Create an empty DataFrame
 df_nodes = pd.DataFrame(columns=['rm_node_name', 'rm_node_uuid'])  
 
 
 # %%
 for i in range(1, len(graph_data['graph'][0]['nodes'])):
-    new_row = {
-        'rm_node_name': graph_data['graph'][0]['nodes'][i]['name'],
-        'rm_node_uuid': graph_data['graph'][0]['cards'][i]['nodeid']
-    }
-    df_nodes = df_nodes.append(new_row, ignore_index=True)
+    # new_row = {
+    #     'rm_node_name': graph_data['graph'][0]['nodes'][i]['name'],
+    #     'rm_node_uuid': graph_data['graph'][0]['nodes'][i]['nodeid']
+    # }
+    new_row = [graph_data['graph'][0]['nodes'][i]['name'], graph_data['graph'][0]['nodes'][i]['nodeid']]
+    print(i)
+    print(new_row)
+    df_nodes.loc[i] = new_row
+    # df_nodes = df_nodes.append(new_row, ignore_index=True)
 
-file_path = 'your_file.tsv'
+outDir = os.path.dirname(os.path.realpath(__file__))
+file_path = outDir + project_name
 df_nodes.to_csv(file_path, sep='\t', index=False)
 
 # len(graph_data['graph'][0]['cards'])
@@ -37,16 +49,11 @@ graph_data['graph'][0]['nodes'][0]['name']
 # %%
 url = "https://raw.githubusercontent.com/achp-project/prj-eamena-marea/main/resource_models/Heritage%20Place.json"
 # Define a regular expression pattern to match the desired substring
-pattern = r'achp-project/(.*?)/main'
 
-# Use re.search() to find the match
-project = re.search(pattern, url)
 
-if project:
-    # Extract the captured group (the part between "achp-project/" and "/main")
-    result = project.group(1)
-    print(result)
-else:
-    print("Pattern not found in the URL")
+
+
+# %%
+print(graph_data['graph'][0]['nodes'][1]['nodeid'])
 
 # %%
