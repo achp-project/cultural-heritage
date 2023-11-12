025fbbf355402953d7f4fd3487905c2675672cc1
zoometh <thomashuet7@gmail.com>
2023-11-12 22:06:37 +0000

    maps

--- map-projects/prj_extent.py
+++ map-projects/prj_extent.py
@@ -1,18 +1,29 @@
-# append data to the GeoJSON project extents
+# %%
 
+import os
+from os.path import isfile, join
+import pandas as pd
 import json
 import requests
-import geopandas as gpd
 
-
-eamena = "https://raw.githubusercontent.com/achp-project/cultural-heritage/main/map-projects/prj-extent/eamena.geojson"
-
-response = requests.get(eamena)
-
-# with open(eamena) as f:
-#     d = json.load(f)
-
-gdf = gpd.read_file(response.text)
-
-# Now you can work with the GeoDataFrame (e.g., visualize it)
-print(gdf.head())
+# map folder
+extents = os.getcwd() + '\\prj-extent\\'
+# reference list
+github_tsv_url = 'https://raw.githubusercontent.com/achp-project/cultural-heritage/main/list.tsv'
+df = pd.read_csv(github_tsv_url, sep='\t')
+# 
+for index, row in df.iterrows():
+	name = row['name']
+	aFile = extents + row['map']
+	print(aFile)
+	# add values
+	logo = row['inst-logo']
+	# map = maps_path + row['map']
+	color = row['color']
+	response = requests.get(aFile)
+	extent = json.loads(response.text)
+	extent["features"][0]["properties"]['logo'] = logo
+	extent["features"][0]["properties"]['color'] = color
+	with open(aFile, "w") as outfile:
+		json.dump(extent, outfile)
+# %%
