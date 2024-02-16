466db4a207147fcc1525ee81486c09d082e7185a
zoometh <thomashuet7@gmail.com>
2024-02-16 20:22:39 +0000

    python SPA

--- graph-parser/graph_rdf.py
+++ graph-parser/graph_rdf.py
@@ -1,23 +1,26 @@
-def to_rdf(	infile = 'https://raw.githubusercontent.com/achp-project/prj-maeasam/main/Site.json', outfile = "C:/Rprojects/achp-ch/graph-parser/temp.ttl"):
+def to_rdf(infile = 'https://raw.githubusercontent.com/achp-project/prj-maeasam/main/Site.json'):
 	# TODO: OWL
 	# inspired from graph_comparator.py
 	import requests
 	from rdflib import Graph, URIRef, Literal, Namespace
 	from rdflib.namespace import RDF, RDFS, DC
 
+	prj = infile.replace("https://raw.githubusercontent.com/achp-project/", "")
+	prj = prj.split('/')[0]
+
 	response = requests.get(infile)
 	data = response.json()
 	nodes = data['graph'][0]['nodes'] # Replace this with the actual nodes data
 	g = Graph()
 
-	MAEASAM = Namespace("http://maeasam#")
+	arches_prj = Namespace("http://" + prj + "#")
 	g.bind("dc", DC)
 	g.bind("rdfs", RDFS)
 	cidoc_crm_ns = "http://www.cidoc-crm.org/cidoc-crm/"
 	g.bind("cidoccrm", Namespace(cidoc_crm_ns))
 
 	for node in nodes:
-		node_uri = MAEASAM[node['nodeid']]
+		node_uri = arches_prj[node['nodeid']]
 		g.add((node_uri, RDF.type, RDFS.Class))
 		g.add((node_uri, DC.title, Literal(node['name'])))
 		g.add((node_uri, RDFS.subClassOf, URIRef(node['ontologyclass'])))
@@ -26,4 +29,4 @@ def to_rdf(	infile = 'https://raw.githubusercontent.com/achp-project/prj-maeasam
 	return str(g.serialize(format='turtle'))
 
 # # to_rdf(outfile = "C:/Rprojects/achp-ch/graph-parser/temp_1.ttl")
-# print(to_rdf())
+print(to_rdf('https://raw.githubusercontent.com/achp-project/prj-eamena-marea/main/resource_models/Heritage%20Place.json'))
